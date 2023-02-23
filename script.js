93e98dba1f21c478d7c9c9856e06be86d377d194
kumarnutan204 <kumarnutan204@gmail.com>
2023-02-24 01:38:12 +0530

    Created html and css files

---
+++ script.js
@@ -0,0 +1,12 @@
+const timeEl= document.getElementById("time");
+const dateEl = document.getElementById("date");
+const currentweatheritemsEl = document.getElementById("current-weather-items");
+const timezone=document.getElementById("time-zone");
+const countryEl = document.getElementById("country");
+const weatherforecastEl = document.getElementById("weather-forecast");
+const currenttempEl =document.getElementById("current-temp");
+
+
+setInterval(() => {
+    
+}, interval);
